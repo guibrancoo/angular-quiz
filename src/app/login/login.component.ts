7b4c885bf6a212ae70507308e6c421f97d090961
Guilherme Branco <guilhermedbranco@outlook.com>
2020-06-25 22:28:32 -0300

    Create Login and Quiz pages

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  listRef: AngularFireList<any>;
+  list: Observable<any[]>;
 
-  ngOnInit(): void {
+  constructor(private db: AngularFireDatabase) {
+    this.listRef = db.list('users');
+    this.list = this.listRef.snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );
+  }
+
+  add(username: string) {
+    this.listRef.push(
+      {
+        user: username
+      }
+    );
+  }
+
+  delete(key: string) {
+    this.listRef.remove(key);
+  }
+
+  ngOnInit() {
   }
 
 }
--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-quiz',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizComponent implements OnInit {
 
-  constructor() { }
+  listRef: AngularFireList<any>;
+  list: Observable<any[]>;
 
-  ngOnInit(): void {
+  constructor(private db: AngularFireDatabase) {
+    this.listRef = db.list('questions');
+    this.list = this.listRef.snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );
+  }
+
+  add(q: string, a: string, a1: string, a2: string, a3: string, a4: string) {
+    this.listRef.push(
+      {
+        question: q,
+        answer: a,
+        alternative1: a1,
+        alternative2: a2,
+        alternative3: a3,
+        alternative4: a4
+      }
+    );
+  }
+
+  delete(key: string) {
+    this.listRef.remove(key);
+  }
+
+  ngOnInit() {
   }
 
 }
